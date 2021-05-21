0da47a40f317a8f921ca1106622ff35e3860b2ca
Deepthi Gunasekara <DGunasekara@users.noreply.github.com>
2021-05-22 02:03:02 +0530

    Update icingawebanddirector_module_icinga.sh
    Auto generating passwords for the mariadb databases and printing them to a  file in the  in /home/password.txt

--- icingawebanddirector_module_icinga.sh
+++ icingawebanddirector_module_icinga.sh
@@ -4,11 +4,15 @@ apt-get install icingaweb2 libapache2-mod-php
 icingacli setup token create
 
 ##mysql icingaweb database
-mysql -u root -pmy_password -e "CREATE DATABASE icingaweb2;CREATE USER icingaweb2@localhost IDENTIFIED BY '###PASSWORD###';GRANT ALL ON icingaweb2.* TO icingaweb2@localhost;Flush privileges;quit"
+mysql_icingaweb_pass=$(date +%s |  base64 | head -c 32)
+echo "Mysql Icingaweb password is $mysql_icingaweb_pass \n" >> /home/passwords.txt
+mysql -u root -p$mysql_root_pass -e "CREATE DATABASE icingaweb2;CREATE USER icingaweb2@localhost IDENTIFIED BY '$mysql_icingaweb_pass';GRANT ALL ON icingaweb2.* TO icingaweb2@localhost;Flush privileges;quit"
 
 
 ##mysql director database
-mysql -u root -pmy_password -e "CREATE DATABASE director CHARACTER SET 'utf8';CREATE USER director@localhost IDENTIFIED BY 'some-password';GRANT ALL ON director.* TO director@localhost;"
+mysql_director_pass=$(date +%s |  base64 | head -c 32)
+echo "Mysql director password is $mysql_director_pass \n" >> /home/passwords.txt
+mysql -u root -p$mysql_root_pass -e "CREATE DATABASE director CHARACTER SET 'utf8';CREATE USER director@localhost IDENTIFIED BY '$mysql_director_pass';GRANT ALL ON director.* TO director@localhost;"
 
 ICINGAWEB_MODULEPATH="/usr/share/icingaweb2/modules"
 REPO_URL="https://github.com/icinga/icingaweb2-module-director"
